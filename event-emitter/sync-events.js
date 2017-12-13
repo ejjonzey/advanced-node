b2299fa0ca07dddee556e74308c69196a5e01034
Ejjonzey <ejjonzey1982@gmail.com>
2017-12-13 12:31:21 -0500

    sync and async events

---
+++ event-emitter/sync-events.js
@@ -0,0 +1,18 @@
+const EventEmitter = require('events');
+
+class WithLog extends EventEmitter {
+    execute(taskFunc) {
+        console.log('Before executing');
+        this.emit('begin');
+        taskFunc();
+        this.emit('end');
+        console.log('after executing');
+    }
+}
+
+const withLog = new WithLog();
+
+withLog.on('begin', () => console.log('about to execute'));
+withLog.on('end', () => console.log('done with execute'));
+
+withLog.execute(() => console.log('*** Executing task ***'));
