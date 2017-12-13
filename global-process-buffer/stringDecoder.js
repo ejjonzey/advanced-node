17adca11235a331bbed958bd45d3241dbfb46dab
Ejjonzey <ejjonzey1982@gmail.com>
2017-12-13 09:35:12 -0500

    utf-8 received as chunks, use StringDecoder

---
+++ global-process-buffer/stringDecoder.js
@@ -0,0 +1,15 @@
+// if receiving utf-8 bytes as chunks, you should always use stringdecoder
+
+const { StringDecoder } = require('string_decoder');
+const decoder = new StringDecoder('utf8');
+
+process.stdin.on('readable', ()=>{
+    const chunk = process.stdin.read();
+    if (chunk != null){
+        const buffer = Buffer.from([chunk]);
+        console.log('With .toString():', buffer.toString());
+        console.log('With StringDecoder:', decoder.write(buffer));
+    }
+});
+
+// 0xE2, 0x82, 0xAC -> €
